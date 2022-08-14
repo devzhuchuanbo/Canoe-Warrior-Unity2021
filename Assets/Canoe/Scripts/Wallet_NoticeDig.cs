f54140b9c0c326b7b6197399038c51440c0fcca1
Bowen <zhuchuanbo@outlook.com>
2022-08-14 18:27:53 +0800

    pref: reborn callback

---
+++ Assets/Canoe/Scripts/Wallet_NoticeDig.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wallet_NoticeDig : MonoBehaviour
+{
+    public Text msg;
+
+    public void ShowAARTTrad()
+    {
+        msg.text = " AART is insufficient \n [Trade to get AART]";
+    }
+
+    public void CanelBtn()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void ConfirmBtn()
+    {
+        WalletController.Instance.WalletStart();
+    }
+
+}
